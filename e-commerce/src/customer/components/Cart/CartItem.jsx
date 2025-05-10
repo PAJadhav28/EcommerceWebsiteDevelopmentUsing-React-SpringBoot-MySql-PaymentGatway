dafdccb3f9bcc1b482ee48e31edb721c767e412d
PallaviJadhav <pallavijadhav2213@gmail.com>
2025-05-10 20:42:21 +0530

    frontend complete

--- e-commerce/src/customer/components/Cart/CartItem.jsx
+++ e-commerce/src/customer/components/Cart/CartItem.jsx
@@ -1,45 +1,83 @@
-import { Button, IconButton } from '@mui/material'
-import React from 'react'
-import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
-import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
-const CartItem = () => {
+import React from "react";
+import { Button } from "@mui/material";
+import { useDispatch } from "react-redux";
+import { removeCartItem, updateCartItem } from "../../../Redux/Customers/Cart/Action";
+import { IconButton } from "@mui/material";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import RemoveCircleOutlineIcon from "@mui/icons-material/RemoveCircleOutline";
+
+const CartItem = ({ item,showButton }) => {
+  const dispatch = useDispatch();
+  const jwt = localStorage.getItem("jwt");
+
+  const handleRemoveItemFromCart = () => {
+    const data = { cartItemId: item?.id, jwt };
+    dispatch(removeCartItem(data));
+  };
+  const handleUpdateCartItem=(num)=>{
+    const data={data:{quantity:item.quantity+num}, cartItemId:item?.id, jwt}
+    dispatch(updateCartItem(data))
+  }
   return (
-    <div className='p-5 shadow-lg border rounded-md'>
-        <div className='flex items-center'>
-            {/* image */}
-            <div className=' w-[5rem] h-[5rem] lg:w-[9rem] lg:h-[9rem]'>
-                <img className='w-full h-full object-cover object-top' src='https://rukminim1.flixcart.com/image/612/612/xif0q/dress/a/x/z/l-na-awd-19-yellow-aarvia-original-imagzffm3bkyzup2.jpeg?q=70' alt=''/>
-            </div>
-
-            {/* content */}
-            <div className='ml-5 space-y-1'>
-                <p className='font-semibold'>dfnfd jk dmd ssc</p>
-                <p className='opacity-70'>size: L,White</p>
-                <p className='opacity-70 mt-2'>dfnfd jk dmd ssc</p>
-                <div className="flex space-x-5 items-center text-lg lg:text-xl text-gray-900 pt-6">
-                  <p className="font-semibold">₹199</p>
-                  <p className="opacity-50 line-through">₹448</p>
-                  <p className="text-green-600 font-semibold">5% off</p>
-                </div>
-            </div>
+    <div className="p-5 shadow-lg border rounded-md">
+      <div className="flex items-center">
+        <div className="w-[5rem] h-[5rem] lg:w-[9rem] lg:h-[9rem] ">
+          <img
+            className="w-full h-full object-cover object-top"
+            src={item?.product.imageUrl}
+            alt=""
+          />
         </div>
-        <div className='lg:flex items-center lg:space-x-10 pt-4'>
-                <div className='flex items-center space-x-2'>
-                    <IconButton>
-                        {/* Button to remove itam */}
-                        <RemoveCircleOutlineIcon/> 
-                    </IconButton>
-                    <span className='py-1 px-7 border rounded-sm' >3</span>
-                        <IconButton sx={{color:"RGB(145 85 253)"}}>
-                            {/* Button to add itam */}
-                            <AddCircleOutlineIcon/>
-                        </IconButton>
-                    </div>
-                <div>
-                    <Button sx={{color:"RGB(145 85 253)"}}>remove</Button></div>
-            </div>
-    </div>
-  )
-}
+        <div className="ml-5 space-y-1">
+          <p className="font-semibold">{item?.product?.title}</p>
+          <p className="opacity-70">Size: {item?.size},White</p>
+          <p className="opacity-70 mt-2">Seller: {item?.product?.brand}</p>
+          <div className="flex space-x-2 items-center pt-3">
+            {/* <p className="opacity-50 line-through">₹{item?.product.price}</p>
+            <p className="font-semibold text-lg">
+              ₹{item?.product.discountedPrice}
+            </p>
+            <p className="text-green-600 font-semibold">
+              {item?.product.discountedPrice}% off
+            </p> */}
+            <p className="opacity-50 line-through">
+              ₹{item?.product?.price * item?.quantity}
+            </p>
+            <p className="font-semibold text-lg">
+              ₹{item?.product?.discountedPrice * item?.quantity}
+            </p>
+            <p className="text-green-600 font-semibold">
+              {Math.round(
+                ((item?.product?.price - item?.product?.discountedPrice) /
+                  item?.product?.price) * 100
+              )}
+              % off
+            </p>
 
-export default CartItem
+          </div>
+        </div>
+      </div>
+     {showButton&& <div className="lg:flex items-center lg:space-x-10 pt-4">
+        <div className="flex items-center space-x-2 ">
+          <IconButton onClick={()=>handleUpdateCartItem(-1)} disabled={item?.quantity<=1} color="primary" aria-label="add an alarm">
+            <RemoveCircleOutlineIcon />
+          </IconButton>
+
+          <span className="py-1 px-7 border rounded-sm">{item?.quantity}</span>
+          <IconButton onClick={()=>handleUpdateCartItem(1)} color="primary" aria-label="add an alarm">
+            <AddCircleOutlineIcon />
+          </IconButton>
+        </div>
+        <div className="flex text-sm lg:text-base mt-5 lg:mt-0">
+          
+          <Button onClick={handleRemoveItemFromCart} variant="text">
+            Remove{" "}
+          </Button>
+          
+        </div>
+      </div>}
+    </div>
+  );
+};
+
+export default CartItem;
